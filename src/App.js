99839984a954a28f24e499482c8dfa275616808b
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-18 18:46:03 -0300

    Correção: Componentização dos elementos.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
+import Header from './components/Header';
+import TabelaClassificacao from './components/TabelaClassificacao';
+import TabelaJogos from './components/TabelaJogos';
 
 export default function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<div className="conteudo">
+				<div>
+					<TabelaJogos />
+					<TabelaClassificacao />
+				</div>
+			</div>
+		</div>
+	);
 }
