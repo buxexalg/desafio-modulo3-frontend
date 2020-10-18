b3b1cf69f8bcad1134fbfc886847648fd6bb1c31
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-18 18:45:14 -0300

    Funcionalidade: Header da página.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,39 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/header.css";
+import '../styles/header.css';
+
+export default function Header() {
+	const [email, setEmail] = React.useState('');
+	const [senha, setSenha] = React.useState('');
+
+	return (
+		<div className="header">
+			<div>
+				<h1>Brasileirão</h1>
+				<div>
+					<form className="login">
+						<label>
+							Email
+							<input
+								type="email"
+								onInput={(event) => {
+									setEmail(event.target.value);
+								}}
+							/>
+						</label>
+						<label>
+							Senha
+							<input
+								type="password"
+								onInput={(event) => {
+									setSenha(event.target.value);
+								}}
+							/>
+						</label>
+						<button type="submit"> Logar </button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
