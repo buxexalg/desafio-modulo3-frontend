059dcf0f8a5709bec97d6ba740fbc4f7e2be92fa
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-24 17:37:34 -0300

    Funcionalidade: Implementada funcionalidade de consumir dados da API e alterar os dados dos times quando autenticado.

--- src/components/TabelaJogos.js
+++ src/components/TabelaJogos.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 import '../styles/tabelaJogos.css';
+import { fazerRequisicaoComBody } from '../utils/requisicoes';
 
-export default function TabelaJogos() {
+export default function TabelaJogos(props) {
 	const [rodada, setRodada] = React.useState(1);
 	const [opacidade1, setOpacidade1] = React.useState(0.5);
 	const [opacidade2, setOpacidade2] = React.useState(1);
 	const [jogos, setJogos] = React.useState([]);
+	const [edicao, setEdicao] = React.useState(null);
+	const [golsCasa, setGolsCasa] = React.useState(null);
+	const [golsVisitante, setGolsVisitante] = React.useState(null);
+	const { token } = props;
+	const { setClassificacao } = props;
 
-	React.useEffect(() => {
-		fetch(
-			`https://desafio-3-back-cubos-academy.herokuapp.com/jogos/${rodada}`,
-			{
-				method: 'GET',
-				headers: {},
-			}
-		)
+	const atualizaJogos = () => {
+		fazerRequisicaoComBody(`http://localhost:8081/jogos/${rodada}`, 'GET')
 			.then((response) => {
 				return response.json();
 			})
@@ -25,6 +25,29 @@ export default function TabelaJogos() {
 			.catch((err) => {
 				console.error(err);
 			});
+	};
+
+	const atualizaClassificacao = () => {
+		fazerRequisicaoComBody(`http://localhost:8081/classificacao`, 'GET')
+			.then((response) => {
+				return response.json();
+			})
+			.then((dados) => {
+				const dadosClassificacao = dados.dados;
+				dadosClassificacao.forEach((elemento, index) => {
+					elemento.id = index + 1;
+					elemento.saldoGols =
+						elemento.golsFeitos - elemento.golsSofridos;
+				});
+				setClassificacao(dadosClassificacao);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
+
+	React.useEffect(() => {
+		atualizaJogos();
 	}, [rodada]);
 
 	return (
@@ -39,7 +62,7 @@ export default function TabelaJogos() {
 							setRodada(rodada - 1);
 							rodada === 2 && setOpacidade1(0.5);
 							rodada === 38 && setOpacidade2(1);
-							setJogos(null);
+							setJogos([]);
 						}
 					}}
 				/>
@@ -54,15 +77,107 @@ export default function TabelaJogos() {
 							setRodada(rodada + 1);
 							rodada === 1 && setOpacidade1(1);
 							rodada === 37 && setOpacidade2(0.5);
-							setJogos(null);
+							setJogos([]);
 						}
 					}}
 				/>
 			</div>
-
 			<div className="conteudoJogos">
-				{jogos === null ? (
-					<div>Carregando...</div>
+				{jogos.length === 0 && (
+					<div className="carregando">Carregando...</div>
+				)}
+				{token ? (
+					<table>
+						<tbody className="tabelaJogos">
+							{jogos.map((elemento, index) => (
+								<tr key={index} className="listaJogo">
+									{edicao !== index ? (
+										<>
+											<td>{elemento.time_casa}</td>
+											<td>{elemento.gols_casa}</td>
+											<td>x</td>
+											<td>{elemento.gols_visitante}</td>
+											<td>{elemento.time_visitante}</td>
+											<td className="celulaEditar">
+												<button
+													className="botaoEditar"
+													onClick={() => {
+														setEdicao(index);
+														setGolsCasa(
+															elemento.gols_casa
+														);
+														setGolsVisitante(
+															elemento.gols_visitante
+														);
+													}}
+												>
+													<img
+														src="https://systemuicons.com/images/icons/pen.svg"
+														alt="Editar"
+													/>
+												</button>
+											</td>
+										</>
+									) : (
+										<>
+											<td>{elemento.time_casa}</td>
+											<td>
+												<input
+													type="numeric"
+													value={golsCasa}
+													className="inputPlacar"
+													onChange={(event) => {
+														setGolsCasa(
+															event.target.value
+														);
+													}}
+												/>
+											</td>
+											<td>x</td>
+											<td>
+												<input
+													type="numeric"
+													value={golsVisitante}
+													className="inputPlacar"
+													onChange={(event) => {
+														setGolsVisitante(
+															event.target.value
+														);
+													}}
+												/>
+											</td>
+											<td>{elemento.time_visitante}</td>
+											<td className="celulaEditar">
+												<button
+													className="botaoEditar"
+													onClick={() => {
+														fazerRequisicaoComBody(
+															'http://localhost:8081/jogos',
+															'POST',
+															{
+																id: elemento.id,
+																golsCasa: golsCasa,
+																golsVisitante: golsVisitante,
+															},
+															token
+														);
+														setEdicao(null);
+														atualizaJogos();
+														atualizaClassificacao();
+													}}
+												>
+													<img
+														src="https://systemuicons.com/images/icons/check.svg"
+														alt="Confirmar edição"
+													/>
+												</button>
+											</td>
+										</>
+									)}
+								</tr>
+							))}
+						</tbody>
+					</table>
 				) : (
 					<table>
 						<tbody className="tabelaJogos">
