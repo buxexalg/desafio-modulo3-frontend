d56a5e9537f7a83a16d32063de78cdc0dd5534fa
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-24 17:35:34 -0300

    Funcionalidade: Adicionadas props de estados para serem utilizadas em mais de um componente.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
+
 import './styles/App.css';
+
 import Header from './components/Header';
 import TabelaClassificacao from './components/TabelaClassificacao';
 import TabelaJogos from './components/TabelaJogos';
 
 export default function App() {
+	const [token, setToken] = React.useState(null);
+	const [classificacao, setClassificacao] = React.useState([]);
+
 	return (
 		<div className="App">
-			<Header />
+			<Header token={token} setToken={setToken} />
 			<div className="conteudo">
 				<div>
-					<TabelaJogos />
-					<TabelaClassificacao />
+					<TabelaJogos
+						token={token}
+						setToken={setToken}
+						classificacao={classificacao}
+						setClassificacao={setClassificacao}
+					/>
+					<TabelaClassificacao
+						classificacao={classificacao}
+						setClassificacao={setClassificacao}
+					/>
 				</div>
 			</div>
 		</div>
