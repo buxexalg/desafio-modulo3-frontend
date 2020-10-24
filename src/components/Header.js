17687adf21eda31fbde7ddebe74f5d0c0c8fadcf
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-10-24 17:37:53 -0300

    Funcionalidade: Login e autenticação implementados.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,66 @@
 import React from 'react';
 
 import '../styles/header.css';
+import { fazerRequisicaoComBody } from '../utils/requisicoes';
 
-export default function Header() {
+export default function Header(props) {
 	const [email, setEmail] = React.useState('');
 	const [senha, setSenha] = React.useState('');
+	const { token, setToken } = props;
 
 	return (
 		<div className="header">
 			<div>
 				<h1>Brasileirão</h1>
 				<div>
-					<form className="login">
-						<label>
-							Email
-							<input
-								type="email"
-								onInput={(event) => {
-									setEmail(event.target.value);
-								}}
-							/>
-						</label>
-						<label>
-							Senha
-							<input
-								type="password"
-								onInput={(event) => {
-									setSenha(event.target.value);
-								}}
-							/>
-						</label>
-						<button type="submit"> Logar </button>
-					</form>
+					{token ? (
+						<button
+							onClick={() => {
+								setToken(null);
+								setEmail('');
+								setSenha('');
+							}}
+						>
+							Deslogar
+						</button>
+					) : (
+						<form
+							className="login"
+							onSubmit={(event) => {
+								event.preventDefault();
+								fazerRequisicaoComBody(
+									'http://localhost:8081/auth',
+									'POST',
+									{
+										email: email,
+										password: senha,
+									}
+								)
+									.then((res) => res.json())
+									.then((tokenJSON) => setToken(tokenJSON));
+							}}
+						>
+							<label>
+								Email
+								<input
+									type="email"
+									onInput={(event) => {
+										setEmail(event.target.value);
+									}}
+								/>
+							</label>
+							<label>
+								Senha
+								<input
+									type="password"
+									onInput={(event) => {
+										setSenha(event.target.value);
+									}}
+								/>
+							</label>
+							<button type="submit"> Logar </button>
+						</form>
+					)}
 				</div>
 			</div>
 		</div>
